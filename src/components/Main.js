45809d2156bf8d233df73c94ce2a7528d683be92
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2020-11-29 12:59:29 +0900

    feature: material ui stylesを使ったグリッド構成

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateRows: '1fr auto',
+  },
+});
 
 const Main = ({ name }) => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       {name}
     </div>
   )
