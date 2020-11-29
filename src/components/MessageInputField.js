cac037ba6c8c5e5e8e263b38fd707e0c8fa2f473
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2020-11-29 13:57:49 +0900

    feature: inputフィールドのマークアップ

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Grid, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '24px',
   },
 });
 
@@ -11,7 +13,13 @@ const MessageInputField = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      MessageInputField
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
     </div>
   )
 }
