d5ff5150e8f020d7fcdeb4baf8ea12b382e0a72d
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2020-12-01 07:04:43 +0900

    feature: 送信フォームのコンポーネント

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Grid, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-import {gravatarPath} from '../gravatar';
+import { gravatarPath } from '../gravatar';
+import MessageField from './MessageField';
 
 const useStyles = makeStyles({
   root: {
@@ -11,18 +12,25 @@ const useStyles = makeStyles({
   },
 });
 
-const MessageInputField = ({ name
-}) => {
+const MessageInputField = ({ name }) => {
+  const [text, setText] = useState('');
   const classes = useStyles();
   const avatarPath = gravatarPath(name);
+  console.log(text);
+
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid xs={1}>
-          <Avatar src={avatarPath
-          } />
+          <Avatar src={avatarPath} />
         </Grid>
-        <Grid xs={10}>入力</Grid>
+        <Grid xs={10}>
+          <MessageField
+            text={text}
+            setText={setText}
+            name={name}
+          />
+        </Grid>
         <Grid xs={1}>ボタン</Grid>
       </Grid>
     </div>
